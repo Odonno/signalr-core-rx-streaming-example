e92ed85e66a779f1fd44a416c2d37bd7581663bb
Odonno <bottiau.david@laposte.net>
2019-12-06 19:45:42 +0100

    feat: add unsubscription in the front app

--- signalr-streaming-client/src/App.tsx
+++ signalr-streaming-client/src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { IStreamResult, HubConnectionBuilder, HttpTransportType } from '@microsoft/signalr';
-import { Subject } from 'rxjs';
+import { HubConnectionBuilder, HttpTransportType, HubConnection } from '@microsoft/signalr';
+import { Subject, interval } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
-const fromStream = <T extends any>(stream: IStreamResult<T>) => {
+const fromStream = <T extends any>(connection: HubConnection, streamName: string, ...args: any[]) => {
   const subject = new Subject<T>();
-  stream.subscribe(subject);
+  connection.stream(streamName, ...args).subscribe(subject);
   return subject.asObservable();
 };
 
@@ -14,30 +14,45 @@ const App: React.FC = () => {
   const [weather, setWeather] = useState<number | undefined>();
 
   useEffect(() => {
+    const hubOptions = {
+      skipNegotiation: true,
+      transport: HttpTransportType.WebSockets
+    };
+
     const connection = new HubConnectionBuilder()
-      .withUrl("https://localhost:44308/weather", { skipNegotiation: true, transport: HttpTransportType.WebSockets })
+      .withUrl("https://localhost:44308/weather", hubOptions)
       .build();
 
     connection.start()
       .then(() => {
-        fromStream(
-          connection.stream<number>("realtimeWeather")
-        ).subscribe(value => setWeather(value));
+        const subscription = fromStream<number>(connection, "realtimeWeather")
+          .subscribe(value => setWeather(value));
+
+        // random unsubscription
+        interval(1000).pipe(
+          map(_ => Math.random() * 100),
+          filter(x => x > 90)
+        ).subscribe(_ => {
+          subscription.unsubscribe();
+        })
       });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo-weather.png" className="App-logo" alt="logo" />
 
         {weather === undefined &&
           <p>Waiting for incoming data...</p>
         }
         {weather !== undefined &&
           <p>
-            Weather: <b>{weather}</b> °C
-        </p>}
+            <b style={{ fontSize: 66 }}>{weather}</b>
+            <span> </span>
+            <span style={{ fontSize: 32 }}>°C</span>
+          </p>
+        }
       </header>
     </div>
   );
